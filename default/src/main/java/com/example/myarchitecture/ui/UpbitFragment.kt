d749dca9a66a061517de35a019d4cc2de70caba8
gangMac <idiosauri@naver.com>
2019-09-15 04:12:18 +0900

    git init

--- default/src/main/java/com/example/myarchitecture/ui/UpbitFragment.kt
+++ default/src/main/java/com/example/myarchitecture/ui/UpbitFragment.kt
@@ -7,9 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.myarchitecture.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class UpbitFragment : Fragment() {
 
     override fun onCreateView(
